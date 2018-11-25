042237f755dd932ea247e7f1de229d105c495fa2
danshafie <danshafie@gmail.com>
2018-11-25 12:21:59 -0800

    initial commit and got data flowing from be to cra

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    data: []
+  };
+  async componentDidMount() {
+    const { data } = await axios.get("http://localhost:3000/");
+    this.setState(data);
+  }
   render() {
+    console.log("this state: ", this.state);
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and poop
           </p>
           <a
             className="App-link"
